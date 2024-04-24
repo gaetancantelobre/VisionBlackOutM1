7e372950ba4950216a68357f869dc2165213febc
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2024-04-24 11:44:30 +0200

    added unit tests for angle calulculation

--- 3D representation/vector_calculation.py
+++ 3D representation/vector_calculation.py
@@ -15,6 +15,13 @@ def get_direction_vector(ball_position, image_width, fov_degrees, distance):
 
 
 def get_object_angle(object_position, image_width, fov_degrees):
+    if(object_position < 0 or object_position >= image_width ):
+        raise ValueError("object position is out of bounds of cam resolution.")
+    if(fov_degrees > 360 or fov_degrees <= 0):
+        raise ValueError("camera FOV is too low or too high.")
+    if(image_width <= 0):
+        raise ValueError("image_width_is_too_low")
+        
     # Calculate the angle of the ball relative to the center of the image
     angle_degrees = ((object_position - image_width / 2) /
                      (image_width / 2)) * (fov_degrees / 2)
